c2001215ed2fb2775f9434fda31e60749118f376
Alexander Haas <mail@haas-alexander.com>
2023-08-09 13:04:33 +0200

    feat: added user input
    now the program can be used as a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use lexer::get_token_stream;
 
 use crate::parser::Parser;
@@ -6,8 +8,13 @@ mod lexer;
 mod parser;
 
 fn main() {
-    let source = "(23-43.1)/7.23e-13*12";
-    let tokens = get_token_stream(source);
+    // let source = "(23-43.1)/7.23e-13*12";
+    println!("Enter some equation using *,/,+,-,( and )");
+    let mut source = String::new();
+    io::stdin()
+        .read_line(&mut source)
+        .expect("failed reading line from stdin");
+    let tokens = get_token_stream(&source);
     println!("Tokenstream: {tokens:?}");
     let parser = Parser::new(tokens);
     let ast = parser.parse();
